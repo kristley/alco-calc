794d69fb9132f0ccf6c436ac74982089dce509b3
Kristoffer Leyton <kriss.1999@hotmail.com>
2022-11-10 15:50:34 +0100

    Added some tests and set up testing

---
+++ App.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import TestRenderer from 'react-test-renderer';
+
+import App from './App';
+
+describe('<App />', () => {
+  it('has 1 child', () => {
+    const tree = TestRenderer.create(<App />).toJSON();
+    expect(tree.children).toBe(null);
+  });
+});
+
