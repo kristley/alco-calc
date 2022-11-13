7d8823212ed5a1298fcd28dcf08732fb83763fbc
mariusbolstad <mariumbo@stud.ntnu.no>
2022-11-13 13:01:20 +0100

    fix percentage placeholder

--- src/screens/addbeverage/components/beverageinput/BeveragePercentageInput.jsx
+++ src/screens/addbeverage/components/beverageinput/BeveragePercentageInput.jsx
@@ -20,27 +20,38 @@ export default function BeveragePercentageInput({ displayedDrink, setDisplayedDr
 		<View style={styles.container}>
 			<TextInput
 				style={styles.textInput}
-				placeholder="0 %"
+				placeholder="0"
 				value={percentage}
 				onChangeText={updatePercentage}
 				maxLength={5}
 			/>
+			<Text style={styles.percentage}>%</Text>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
-		width: 53,
+		width: 60,
 		height: 27,
 		backgroundColor: "#E6E6E6",
 		borderRadius: 15,
-	},
+		flexDirection: "row",
+		textAlignVertical: "center"
+		},
 	textInput: {
 		top: 0,
 		left: 13,
 		color: "#121212",
 		height: 27,
 		width: 52,
+		flex: 2
 	},
+	percentage:{
+		color: "#121212",
+		flex: 1,
+		textAlignVertical: "center",
+		top: 5
+
+	}
 });
