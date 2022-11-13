590186b919b2e00c86d6749d19afa5c7f812e13a
Lars Overskeid <larsover@stud.ntnu.no>
2022-11-13 16:29:44 +0100

    started fixing merge conflicts

--- src/screens/addbeverage/components/beverageinput/BeverageInput.jsx
+++ src/screens/addbeverage/components/beverageinput/BeverageInput.jsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { StyleSheet, View, TextInput } from "react-native";
+import { StyleSheet, View } from "react-native";
 import BeverageNameInput from "./BeverageNameInput";
 import BeveragePercentageInput from "./BeveragePercentageInput";
 import BeverageVolumeInput from "./BeverageVolumeInput";
 import AddBeverageButton from "./AddBeverageButton";
 import BeverageUnitInput from "./BeverageUnitInput";
-import DisplayMode from "react-native/Libraries/ReactNative/DisplayMode";
-import { back } from "react-native/Libraries/Animated/Easing";
 
 export default function BeverageInput({ displayedDrink, setDisplayedDrink, navigation }) {
     return (
@@ -44,13 +42,6 @@ export default function BeverageInput({ displayedDrink, setDisplayedDrink, navig
                 />
             </View>
         </View>
-        <View style={styles.beverageOthersContainer}>
-            <BeveragePercentageInput style={styles.beveragePercentageInput}></BeveragePercentageInput>
-            <BeverageVolumeInput style={styles.beverageVolumeInput}></BeverageVolumeInput>
-            <BeverageUnitInput style={styles.beverageUnitInput}></BeverageUnitInput>
-            <AddBeverageButton style={styles.addBeverageButton} navigation={navigation}></AddBeverageButton>
-        </View>
-     </View>
     )
 }
 
@@ -59,37 +50,37 @@ const styles = StyleSheet.create({
         flex: 1,
         alignSelf: "center",
         maxWidth: 500,
-        },
-    beverageNameContainer:{
+    },
+    beverageNameContainer: {
         flex: 1,
         borderRadius: 15,
         flexDirection: "row",
         maxWidth: 500,
         marginHorizontal: 20,
         justifyContent: "center"
-        },
-    beverageNameInput:{
+    },
+    beverageNameInput: {
         height: 35,
         flex: 1,
         borderRadius: 15,
         alignSelf: "center"
     },
-    beverageOthersContainer:{
+    beverageOthersContainer: {
         flex: 1,
         borderRadius: 15,
         flexDirection: "row",
         marginHorizontal: 20,
         justifyContent: "space-between"
     },
-    beveragePercentageInput:{
+    beveragePercentageInput: {
         width: 53,
         height: 27,
         flex: 1,
         backgroundColor: "#E6E6E6",
         borderRadius: 15,
         padding: 20
-       },
-   beverageVolumeInput:{
+    },
+    beverageVolumeInput: {
         width: 53,
         height: 27,
         flex: 1,
