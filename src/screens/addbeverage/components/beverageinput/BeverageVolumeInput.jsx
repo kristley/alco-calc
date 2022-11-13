cd3fc172c4455c7c7ce72836f0556db140dc4a4b
mariusbolstad <mariumbo@stud.ntnu.no>
2022-11-13 13:04:54 +0100

    Fix volume placeholder

--- src/screens/addbeverage/components/beverageinput/BeverageVolumeInput.jsx
+++ src/screens/addbeverage/components/beverageinput/BeverageVolumeInput.jsx
@@ -1,27 +1,37 @@
 import React from "react";
-import { StyleSheet, View, TextInput } from "react-native";
+import { StyleSheet, View, TextInput, Text } from "react-native";
 
 
 export default function BeverageVolumeInput() {
   return (
     <View style={styles.container}>
-      <TextInput placeholder="0ml" style={styles.textInput}></TextInput>
+      <TextInput placeholder="0" style={styles.textInput}></TextInput>
+      <Text style={styles.volume}>L</Text>
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    width: 53,
-    height: 27,
-    backgroundColor: "#E6E6E6",
-    borderRadius: 15
-  },
+		width: 60,
+		height: 27,
+		backgroundColor: "#E6E6E6",
+		borderRadius: 15,
+		flexDirection: "row",
+		textAlignVertical: "center"
+		},
   textInput: {
-    top: 0,
-    left: 13,
-    color: "#121212",
-    height: 27,
-    width: 52
+		top: 0,
+		left: 13,
+		color: "#121212",
+		height: 27,
+		width: 52,
+		flex: 2
+	},
+	volume:{
+		color: "#121212",
+		flex: 1,
+		textAlignVertical: "center",
+		top: 5
   }
 });
