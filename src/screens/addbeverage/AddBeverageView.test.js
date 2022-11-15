e21004dbec4050f2c2e2f132c2c9f43ef52c610e
Kristoffer Leyton <kriss.1999@hotmail.com>
2022-11-15 21:52:30 +0100

    Changed gitlab-ci to test project

--- src/screens/addbeverage/AddBeverageView.test.js
+++ src/screens/addbeverage/AddBeverageView.test.js
@@ -1,11 +1,27 @@
 import React from 'react';
 import TestRenderer from 'react-test-renderer';
 import AddBeverageView from './AddBeverageView';
-import App from '../../../App';
 
-describe('', () => {
-  it('has 1 child', () => {
-    const tree = TestRenderer.create(<AddBeverageView />).toJSON();
-    expect(tree.children.length).toBe(4);
-  });
+describe('AddBeverageView', () => {
+  describe('When rendered', () => {
+    it('has 1 child', () => {
+      const tree = TestRenderer.create(<AddBeverageView />).toJSON();
+      expect(tree.children.length).toBe(4);
+    });
+  })
+
+  describe('Adds beverage to list', () => {
+    it('adds beverage to list', () => {
+      const tree = TestRenderer.create(<AddBeverageView />).toJSON();
+      expect(tree.children.length).toBe(5);
+    });
+  })
+
+  describe('Bad inputs are not allowed', () => {
+
+  })
+
+  describe('Fills in from prefab', () => {
+
+  })
 });
