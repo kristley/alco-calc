5772cff986a0e5812a0aaca03609733be70b873f
Kristoffer Leyton <kriss.1999@hotmail.com>
2022-11-17 15:08:54 +0100

    Topbar no longer adjusts itself when switching dates

--- src/screens/alcocalc/components/TopBar.jsx
+++ src/screens/alcocalc/components/TopBar.jsx
@@ -3,59 +3,61 @@ import { StyleSheet, View, Text } from "react-native";
 import ArrowButton from "./ArrowButton";
 
 export default function TopBar({ displayedDate, setDisplayedDate, today }) {
+	const [readableDate, setReadableDate] = useState("");
 
-  const [readableDate, setReadableDate] = useState("");
+	const updateReadableDate = () => {
+		if(displayedDate == today) {
+			setReadableDate("Today");
+			return;
+		}
+		const datestring = new Date(
+			displayedDate.substring(0, 4),
+			displayedDate.substring(5, 6) - 1,
+			displayedDate.substring(7, 8)
+		).toDateString();
+		setReadableDate(datestring);
+	};
 
-  const updateReadableDate = () => {
-    const datestring = new Date(displayedDate.substring(0, 4), displayedDate.substring(5, 6) - 1, displayedDate.substring(7, 8)).toDateString()
-    setReadableDate(datestring);
-  }
+	useEffect(() => {
+		updateReadableDate();
+	}, [displayedDate]);
 
-  useEffect(() => {
-    updateReadableDate();
-  }, [displayedDate]);
+	return (
+		<View style={styles.topBar}>
+			<ArrowButton
+				symbol="chevron-thin-left"
+				displayedDate={displayedDate}
+				setDisplayedDate={setDisplayedDate}
+				forward={false}
+				today={today}
+			/>
 
-  return (
-    <View style={styles.topBar}>
-      <ArrowButton
-        symbol="chevron-thin-left"
-        displayedDate={displayedDate}
-        setDisplayedDate={setDisplayedDate}
-        forward={false}
-        today={today}
-      />
+			<Text style={styles.date}>
+				{readableDate}
+			</Text>
 
-      <Text style={styles.date}>
-        {
-          displayedDate == today ?
-            "Today" :
-            readableDate
-        }
-      </Text>
-
-      <ArrowButton
-        symbol="chevron-thin-right"
-        displayedDate={displayedDate}
-        setDisplayedDate={setDisplayedDate}
-        forward={true}
-        today={today}
-      />
-    </View>
-  );
+			<ArrowButton
+				symbol="chevron-thin-right"
+				displayedDate={displayedDate}
+				setDisplayedDate={setDisplayedDate}
+				forward={true}
+				today={today}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  topBar: {
-    flexDirection: "row",
-    alignItems: "center",
-    justifyContent: "space-between",
-    padding: 15,
-    marginHorizontal: 20,
-    backgroundColor: "#eee",
-    borderRadius: 20,
-  },
-  date: {
-    // textAlign : "center",
-    fontSize: 30,
-  }
+	topBar: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		padding: 15,
+		marginHorizontal: 20,
+		backgroundColor: "#eee",
+		borderRadius: 20,
+	},
+	date: {
+		fontSize: 24,
+	},
 });
