9adabc1fa45e7d82315fd5d4d57974833259b4d2
Kristoffer Leyton <kriss.1999@hotmail.com>
2022-11-22 20:45:57 +0100

    Breaking change js->ts

--- App.tsx
+++ App.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import 'react-native-gesture-handler';
 import React from "react";
 import { StyleSheet } from "react-native";
@@ -23,6 +24,44 @@ function AppContainer() {
 		</Stack.Navigator>
 	);
 }
+
+// Import the functions you need from the SDKs you need
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
+
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyA-nJsv70aEY97lR9GDhfuJEbwY2lrkbUk",
+
+  authDomain: "alcocalc-a019a.firebaseapp.com",
+
+  projectId: "alcocalc-a019a",
+
+  storageBucket: "alcocalc-a019a.appspot.com",
+
+  messagingSenderId: "20427422226",
+
+  appId: "1:20427422226:web:4ae1313a6ccaff3a5bb6cb",
+
+  measurementId: "G-ZEF4DYE8PZ"
+
+};
+
+
+// Initialize Firebase
+
+const app = initializeApp(firebaseConfig);
+
+const auth = getAuth(app);
+
+const db = getFirestore(app);
 
 export default function App() {
 	return (
